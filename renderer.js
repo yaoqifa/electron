47f1e2dc07c282d9ce1e27e0d1a19ee87a416c25
yaoqifa <yaoqifa07330@hellobike.com>
2019-06-17 16:51:02 +0800

    electron start

---
+++ renderer.js
@@ -0,0 +1,12 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+let myNotification = new Notification('标题', {
+  body: '通知正文内容'
+})
+
+myNotification.onclick = () => {
+  console.log('通知被点击')
+}
+
+console.log(app.dock)
